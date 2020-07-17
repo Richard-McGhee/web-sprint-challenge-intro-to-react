67838f2e330b1da6c336c7cef37d78db60b9ebe8
richard-mcghee <richardjmcghee@yahoo.com>
2020-07-17 12:00:21 -0400

    setup basics of components and props, succesfully implented api request

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,11 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Character (props) {
+    console.log(props)
+
+    return (
+        <p></p>
+    )
+}
