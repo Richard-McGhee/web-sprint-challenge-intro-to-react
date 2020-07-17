64c2e2b524df9c90fc593d6e4be1dce558e93bec
richard-mcghee <richardjmcghee@yahoo.com>
2020-07-17 13:13:48 -0400

    more stylistic changes

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -8,7 +8,7 @@ const Container = styled.div`
     justify-content: space-around;
     width: 100%;
     height: 100vh;
-
+    
     p {
         width: 40%;
         height: 10vh;
@@ -24,13 +24,9 @@ export default function Character (props) {
     return (
         <Container>
             { 
-                    props.characterData.map((item) => (
-                        
-                        <p key={item.name}>
-                        {`NAME: ${item.name} HEIGHT: ${item.height} HAIR COLOR: ${item.hair_color} EYE COLOR: ${item.eye_color}`}
-                        </p>
-                    
-                    ))
+                props.characterData.map((item) => (
+                    <p key={item.name}>{`NAME: ${item.name} HEIGHT: ${item.height} HAIR COLOR: ${item.hair_color} EYE COLOR: ${item.eye_color}`}</p>
+                ))
             }
         </Container>
     )
